0bf13b4b9a0826c314eafb7576d65afd651afa3a
Eng-AlaaElgharably <115916463+Eng-AlaaElgharably@users.noreply.github.com>
2024-11-18 18:14:05 +0200

    Update next.config.mjs
    edit next config to display the images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+  basePath: '/powerof', // Replace 'repo-name' with your GitHub repository name
+};
 export default nextConfig;
