a2a93ae4a34d9e6002df789a38cac805bba78e8f
Eng-AlaaElgharably <115916463+Eng-AlaaElgharably@users.noreply.github.com>
2024-11-18 18:47:57 +0200

    Update companies.js

--- src/components/companies.js
+++ src/components/companies.js
@@ -10,36 +10,36 @@ export default function Companies(){
                     </div>
                     <div className="g-c-12 slideshow-container">
                         <div className="slideshow">
-                            <Image src="/images/c1.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c2.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c3.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c4.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c5.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c6.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c7.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c8.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c9.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c10.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c12.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c13.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c14.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c15.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c16.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c17.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c18.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c19.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c20.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c21.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c22.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c23.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c24.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c25.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c26.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c27.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c28.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c29.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c30.jpeg" alt="company" width={200} height={100}></Image>
-                            <Image src="/images/c31.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c1.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c2.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c3.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c4.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c5.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c6.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c7.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c8.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c9.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c10.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c12.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c13.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c14.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c15.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c16.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c17.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c18.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c19.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c20.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c21.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c22.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c23.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c24.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c25.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c26.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c27.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c28.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c29.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c30.jpeg" alt="company" width={200} height={100}></Image>
+                            <Image src="images/c31.jpeg" alt="company" width={200} height={100}></Image>
                         </div>
                     </div>
                 </div>
