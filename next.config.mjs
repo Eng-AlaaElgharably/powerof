0864c42def03662a7abde9e8813156d72bdd0db4
Eng-AlaaElgharably <115916463+Eng-AlaaElgharably@users.noreply.github.com>
2024-11-18 18:18:02 +0200

    Update next.config.mjs
    second update for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/powerof', // Replace 'repo-name' with your GitHub repository name
+  assetPrefix: '/repo-name', // Add the same prefix for assets
+
 };
 export default nextConfig;
