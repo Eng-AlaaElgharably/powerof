92ebfab59fd057de277551ad1cde36fd93720df4
Eng-AlaaElgharably <115916463+Eng-AlaaElgharably@users.noreply.github.com>
2024-11-18 18:49:16 +0200

    Update products.js

--- src/components/products.js
+++ src/components/products.js
@@ -10,55 +10,55 @@ export default function Products(){
                     </div>
                     <div className="g-c-12 f-c-c f-w-w">
                         <div className="product">
-                            <Image src="/images/p1.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p1.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>نقاط التجمع والعلامات الإرشادية</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p2.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p2.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة الإطفاء الآلي بالغازات</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p3.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p3.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>مراوح الدخان والتهوية</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p4.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p4.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة الإطفاء بالمحلول الكيميائي</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p5.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p5.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة ستائر الدخان/الحريق</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p6.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p6.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>الأبواب المقاومة للحديد</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p7.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p7.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>إنارة ومخارج الطوارئ</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p8.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p8.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة إنذار الحريق</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p9.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p9.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة الإطفاء برذاذ الماء</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p10.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p10.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة كاميرات المراقبة</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p11.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p11.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>محطات مضخات الحريق</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p12.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p12.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>أنظمة الإطفاء الآلي بالماء والرغوة</p></div>
                         </div>
                         <div className="product">
-                            <Image src="/images/p13.jpeg" alt="power of product" width={150} height={150}></Image>
+                            <Image src="images/p13.jpeg" alt="power of product" width={150} height={150}></Image>
                             <div className="f-c-c"><p>خراطيم وعساكر وطفايات الحريق</p></div>
                         </div>
                     </div>
