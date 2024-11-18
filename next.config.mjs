481d63b0e926663861af909f6d4c0b0357434b62
Eng-AlaaElgharably <115916463+Eng-AlaaElgharably@users.noreply.github.com>
2024-11-18 18:20:45 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '/powerof', // Replace 'repo-name' with your GitHub repository name
-  assetPrefix: '/repo-name', // Add the same prefix for assets
+  assetPrefix: '/powerof', // Add the same prefix for assets
 
 };
 export default nextConfig;
