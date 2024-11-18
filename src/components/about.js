7a38e1ae7f795cabe541ea5798d70168fa9a291d
Eng-AlaaElgharably <115916463+Eng-AlaaElgharably@users.noreply.github.com>
2024-11-18 18:47:06 +0200

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -13,7 +13,7 @@ export default function About(){
                         <p className="about-text">تأسست شركة قوة الإطفاء في جدة - المملكة العربية السعودية، وهي من بين الشركات الرائدة في المملكة العربية السعودية التي تقدم خدمات ومنتجات أنظمة الحماية من الحرائق وأجهزة إنذار الحريق ومعدات مكافحة الحرائق وأنظمة هندسة الأمن والسلامة. تمتلك شركة قوة الإطفاء فريقًا من المهندسين والفنيين المدربين والمؤهلين بشكل احترافي يعملون على الافا بالتزامنا بتقديم خبراتنا لعملائنا في حماية الأرواح والبيئة والممتلكات.</p>
                     </div>
                     <div className="g-c-6 g-c-l-12 about-image">
-                        <Image src="/images/about.jpg" alt="about power of" layout="fill"></Image>
+                        <Image src="images/about.jpg" alt="about power of" layout="fill"></Image>
                     </div>
                 </div>
             </div>
